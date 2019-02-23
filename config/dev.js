3119395f2337d11850cf1334cc992a38df24ffcd
Gareth <gareth.kh.lau@gmail.com>
2019-02-23 18:56:42 -0500

    removed dev keys

--- config/dev.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-	googleClientID: '110386548233-ml8u368ba54bvic61irjevn885q90i3b.apps.googleusercontent.com',
-	googleClientSecret: 'Q92wypNSpScVRZrRxKhK5hUr',
-	mongoURI: 'mongodb://yummy-dev:yummypass1@ds249798.mlab.com:49798/directory-dev',
-	mongoUser: 'yummy-dev',
-	mongoPassword: 'yummypass1',
-	cookieKey: 'thebestofthebest'
-};
